3c170954816bdb84857453edf2e094d2b1b45d02
Aleksandr Akhmetshin <sashochec2015@gmail.com>
2025-05-13 00:58:06 +0200

    Plot of errors

--- chuo.py
+++ chuo.py
@@ -34,13 +34,17 @@ X_val, y_val = X_val.to_numpy(), y_val.to_numpy()
 X_all, y_all = X.to_numpy(), y.to_numpy()
 
 # Initialize a neural network
-nn = NeuralNet.NeuralNet(hidden_layer_sizes=(16, 8, 16), activation='tahn', learning_rate=0.01, epoch=200, batch_size=1)
+nn4 = NeuralNet.NeuralNet(hidden_layer_sizes=(16, 4, 16), activation='tahn', learning_rate=0.01, epoch=200, batch_size=5)
+nn8 = NeuralNet.NeuralNet(hidden_layer_sizes=(16, 8, 16), activation='tahn', learning_rate=0.01, epoch=200, batch_size=5)
+nn12 = NeuralNet.NeuralNet(hidden_layer_sizes=(16, 12, 16), activation='tahn', learning_rate=0.01, epoch=200, batch_size=5)
 
 # Fit the model
-nn.fit(X_train, y_train, X_val, y_val)
+nn4.fit(X_train, y_train, X_val, y_val)
+nn8.fit(X_train, y_train, X_val, y_val)
+nn12.fit(X_train, y_train, X_val, y_val)
 
-with open('neural_net_tp4.txt', 'w') as nn_file:
-    print(nn, file=nn_file)
+# with open('neural_net_tp4.txt', 'w') as nn_file:
+#     print(nn, file=nn_file)
 
 
 # Find 50% best instances
@@ -52,32 +56,14 @@ X_50 = X_all[mask, :]
 labels_50 = labels[mask]
 
 # Make the predictions for best 50%
-X_50_compressed = nn.compresse(X_50)
+X_50_compressed_to4 = nn4.compresse(X_50)
+X_50_compressed_to8 = nn8.compresse(X_50)
+X_50_compressed_to12 = nn12.compresse(X_50)
 
 
-# PCA graphiques
+# PCA graphiques for 8-compressing
 pca = PCA(n_components=2)
 unique_labels = np.unique(labels)
-
-# PCA graphique for initial data (all)
-X_pca = pca.fit_transform(X_all)
-plt.figure(figsize=(12, 8))
-
-for lab in unique_labels:
-    mask = labels == lab
-    plt.scatter(
-        X_pca[mask, 0],
-        X_pca[mask, 1],
-        label= f"Espese {lab}",
-        alpha=0.7
-    )
-
-plt.title('PCA for all data')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
-plt.legend()
-plt.grid(True)
-plt.show()
 
 # PCA graphique for initial data (50% best)
 X_50_pca = pca.fit_transform(X_50)
@@ -101,7 +87,7 @@ plt.show()
 
 
 # PCA graphique for compressed data (50% best)
-X_50_comp_pca = pca.fit_transform(X_50_compressed)
+X_50_comp_pca = pca.fit_transform(X_50_compressed_to8)
 
 plt.figure(figsize=(12, 8))
 for lab in unique_labels:
@@ -119,3 +105,48 @@ plt.ylabel('PC2')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+
+
+# Functions pour l'erreur moyeene et son ecart-type
+def er_instance(X, y, nn):
+    return None
+
+def er_attribute(X, y, nn):
+    return None
+
+def er_classe(X, y, nn):
+    return None
+
+def plot_model_comparison(type):
+    if type == 'instance':
+        error_f = er_instance
+    elif type == 'attribute':
+        error_f = er_attribute
+    elif type == 'classe':
+        error_f = er_classe
+
+    # array of errors for 3 models
+    errors = [error_f(X_all, y_all, nn4), error_f(X_all, y_all, nn8), error_f(X_all, y_all, nn12)]
+    errors_names = [f"Model compressing to {4 * i} features" for i in range(1, 4)]
+
+    # Mean and ecart-type
+    means = [np.mean(m) for m in errors]
+    stds = [np.std(m) for m in errors]
+
+    # Drawing plot
+    plt.figure(figsize=(12, 8))
+    plt.errorbar(errors_names, means, yerr=stds, fmt='o', capsize=5, color='black')
+
+    plt.title(f"Comparing of {type}s errors for 3 different models")
+    plt.ylabel('Value')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    plt.show()
+
+
+plot_model_comparison('instance')
+plot_model_comparison('attribute')
+plot_model_comparison('classe')
+
+
